779b90d755e90731cd3e4a7b09a50af9b2f72612
TsengYiYao <yiyatsen@gmail.com>
2019-12-23 17:06:53 +0800

    184. The ListView Challenge mine

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -50,6 +50,7 @@ class TasksScreen extends StatelessWidget {
           ),
           Expanded(
             child: Container(
+              child: TodoeyListView(),
               decoration: BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.only(
@@ -61,6 +62,64 @@ class TasksScreen extends StatelessWidget {
           ),
         ],
       ),
+    );
+  }
+}
+
+class TodoeyListView extends StatelessWidget {
+  const TodoeyListView({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: const <Widget>[
+        Card(child: ListTile(title: Text('One-line ListTile'))),
+        Card(
+          child: ListTile(
+            leading: FlutterLogo(),
+            title: Text('One-line with leading widget'),
+          ),
+        ),
+        Card(
+          child: ListTile(
+            title: Text('One-line with trailing widget'),
+            trailing: Icon(Icons.more_vert),
+          ),
+        ),
+        Card(
+          child: ListTile(
+            leading: FlutterLogo(),
+            title: Text('One-line with both widgets'),
+            trailing: Icon(Icons.more_vert),
+          ),
+        ),
+        Card(
+          child: ListTile(
+            title: Text('One-line dense ListTile'),
+            dense: true,
+          ),
+        ),
+        Card(
+          child: ListTile(
+            leading: FlutterLogo(size: 56.0),
+            title: Text('Two-line ListTile'),
+            subtitle: Text('Here is a second line'),
+            trailing: Icon(Icons.more_vert),
+          ),
+        ),
+        Card(
+          child: ListTile(
+            leading: FlutterLogo(size: 72.0),
+            title: Text('Three-line ListTile'),
+            subtitle:
+                Text('A sufficiently long subtitle warrants three lines.'),
+            trailing: Icon(Icons.more_vert),
+            isThreeLine: true,
+          ),
+        ),
+      ],
     );
   }
 }
